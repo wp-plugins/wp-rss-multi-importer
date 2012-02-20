af8b367fd8d12fa9abc18ee31117b83411a2e98a
amweiss98 <amweiss98@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-02-20 20:55:14 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wp-rss-multi-importer/trunk@507887 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-rss-multi-importer/uninstall.php
+++
@@ -1,8 +0,0 @@
-<?php
-// If uninstall not called from WordPress exit
-if( !defined( 'WP_UNINSTALL_PLUGIN' ) )
-exit ();
-// Delete option from options table
-delete_option( 'rss_import_items' );
-//
-?>
