fa620a1c5badce41d37265be4a0179c23e691500
amweiss98 <amweiss98@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-14 03:00:57 +0000

    3.00 tag
    git-svn-id: https://plugins.svn.wordpress.org/wp-rss-multi-importer/tags/3.00.00@965508 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inc/export.php
@@ -0,0 +1,139 @@
+<?php
+function wp_rss_multi_importer_feed_page() {
+
+       ?>
+		<div id="icon-themes" class="icon32 icon32-posts-rssmi_feed"></div>
+	<h2><?php  _e("Multi-Importer", 'wp-rss-multi-importer')?></h2>
+       <div class="wrap">
+	  <h2><?php _e("Export Feeds", 'wp-rss-multi-importer')?></h2>
+	<div id="poststuff">
+
+
+       <div id="options">
+
+       <form action="options.php" method="post"  >            
+
+       <?php
+
+      settings_fields('wp_rss_multi_importer_feed_options');
+      $options = get_option('rss_feed_options');    
+
+       ?>
+
+
+<div class="postbox">
+	
+<div class="inside">
+
+
+<h2><?php _e("Export Your Feeds as an RSS Feed", 'wp-rss-multi-importer')?></h3>
+<h3><?php _e("Export Feed Options Settings", 'wp-rss-multi-importer')?></h3>
+<p><?php _e("You can re-export your feeds as an RSS feed for your readers.  You configure some options for this feed here.", 'wp-rss-multi-importer')?></p>
+
+<p><label class='o_textinput' for='feedtitle'><?php _e("Feed Title", 'wp-rss-multi-importer')?></label>
+
+<input id="feedtitle" type="text" value="<?php echo $options['feedtitle']?>" name="rss_feed_options[feedtitle]"></p>
+
+<p><label class='o_textinput' for='feedslug'><?php _e("Feed Slug", 'wp-rss-multi-importer')?></label>
+
+<input id="feedslug" size="10" type="text" value="<?php echo $options['feedslug']?>" name="rss_feed_options[feedslug]"> <?php _e("(no spaces are allowed!  See what a slug is below)", 'wp-rss-multi-importer')?></p>
+
+<p><label class='o_textinput' for='feeddesc'><?php _e("Feed Description", 'wp-rss-multi-importer')?></label>
+
+<input id="feeddesc" type="text" value="<?php echo $options['feeddesc']?>" name="rss_feed_options[feeddesc]" size="50"></p>
+
+<p><label class='o_textinput' for='striptags'><?php _e("Check to get rid of all images in the feed output.", 'wp-rss-multi-importer')?><input type="checkbox" Name="rss_feed_options[striptags]" Value="1" <?php if (isset($options['striptags']) && $options['striptags']==1){echo 'checked="checked"';} ?></label>
+</p>
+
+       <p class="submit"><input type="submit" value="Save Settings" name="submit" class="button-primary"></p>
+
+       </form>
+
+	<?php
+	$url=site_url();
+	if (!empty($options['feedslug'])){
+
+	echo "<h3>". __("Your RSS feed is here:", 'wp-rss-multi-importer'). "<br><br><a href=".$url."?feed=".$options['feedslug']." target='_blank'>".$url."?feed=".$options['feedslug']."</a></h3>";
+
+	
+		echo "<p>". __("To activate this feature, you may need to save your permalinks again by going to Settings -> Permalinks and clicking Save Changes.", 'wp-rss-multi-importer'). "</p>";
+	}else{
+		
+		echo "<h3>". __("Your RSS feed is here:", 'wp-rss-multi-importer')." <br><br>".$url."?feed=[this is your slug]</h3>";
+	}
+
+	?>
+
+
+</div></div>
+
+<div class="postbox">
+	
+<div class="inside">
+
+  <h2><?php _e("Export Your Feeds as a Text File", 'wp-rss-multi-importer')?></h2>
+<p><?php _e("Export a text file with all of the feed URLS you have stored in this plugin", 'wp-rss-multi-importer')?> </p>
+	<form method="post">
+      <p class="submit">
+	
+
+    
+          <input type="submit" name="rssmi_export_feeds" value="<?php _e( 'Export Your Feeds', 'wp-rss-multi-importer' ); ?>"  class="button" />
+	Check to include all fields (title, URL, Category, User)<input type="checkbox" Name="rssmi_allInfo" Value="1">
+      </p>
+  </form>
+</div></div>
+
+</div></div>
+<?php
+}
+
+
+
+
+
+
+
+function wp_rssmi_download_feed_stream($i){
+
+	$rssmi_url_download='';
+	global $wpdb;
+	$myarray = array();
+	$feed_array=$wpdb->get_results("SELECT ID FROM $wpdb->posts WHERE  post_type ='rssmi_feed' AND post_status='publish'");
+	foreach ($feed_array as $feed){ 
+		
+		if ($i==1){
+			
+			$rssmi_url_download .= get_the_title( $feed->ID).","; 
+			$rssmi_url_download .= get_post_meta($feed->ID, 'rssmi_url', true ).","; 
+			$rssmi_url_download .= get_post_meta($feed->ID, 'rssmi_cat', true ).",";
+			$rssmi_url_download .= get_post_meta($feed->ID, 'rssmi_user', true );	
+		}else{
+			$rssmi_url_download .= get_post_meta($feed->ID, 'rssmi_url', true ); 
+		}
+		
+		$rssmi_url_download .="\n" ;
+	
+	}
+	echo $rssmi_url_download;
+	die();	
+}
+
+add_action( 'admin_init', 'wp_rssmi_download_feeds', 1 );
+
+
+function wp_rssmi_download_feeds() {
+    if ( isset( $_POST['rssmi_export_feeds'] ) ) {  //watch for post
+	$i=((isset($_POST['rssmi_allInfo']) && $_POST['rssmi_allInfo']==1)? 1:0);
+        $file_name = "feeds.txt";
+        header( 'Content-Description: File Transfer' );
+        header( "Content-Type: text/plain; charset=" . get_option( 'blog_charset' ) );
+        header( "Content-Disposition: attachment; filename=$file_name." );
+        wp_rssmi_download_feed_stream($i);
+        die();
+    }
+}
+
+
+
+?>
---
+++ inc/import_feeds.php
@@ -0,0 +1,365 @@
+<?php
+/*
+Feed Item Imports - The code on this page was adapted and expanded from an earlier version of WP RSS Aggregator by jeangalea
+*/
+
+function rssmi_fetch_all_feed_items( ) {     
+	
+	$rssmi_global_options = get_option('rssmi_global_options'); 
+	$single_feed_max=(isset($rssmi_global_options['single_feed_max']) ? $rssmi_global_options['single_feed_max'] : 20);
+       
+	$directFetch=1; 
+	$timeout=20;
+	$forceFeed=true;
+	$showVideo=0;
+	
+        // Get all feed sources
+        $feed_sources = new WP_Query( array(
+            'post_type'      => 'rssmi_feed',
+            'post_status'    => 'publish',
+            'posts_per_page' => -1,
+        ) );
+
+        if( $feed_sources->have_posts() ) {
+            /* Get feed sources*/
+            while ( $feed_sources->have_posts() ) {     
+	            $didUpdate=0;
+                $feed_sources->the_post();
+                $feed_ID = get_the_ID();
+                $feed_url = get_post_meta( get_the_ID(), 'rssmi_url', true );
+				$feed_cat = get_post_meta( get_the_ID(), 'rssmi_cat', true );
+				$rssmi_user= get_post_meta(get_the_ID(), 'rssmi_user', true );
+				$rssmi_title= get_the_title(get_the_ID());
+                    
+                // Use the URL custom field to fetch the feed items for this source
+                if( !empty( $feed_url ) ) {    
+					
+							$url = esc_url_raw(strip_tags($feed_url));
+	
+							if ($directFetch==1){
+								$feed = wp_rss_fetchFeed($url,$timeout,$forceFeed,$showVideo);
+							}else{
+								$feed = fetch_feed($url);
+							}
+					
+					         
+                    if ( !is_wp_error( $feed ) ) {
+                        // Limit items it to 10. 
+                        $maxitems = $feed->get_item_quantity($single_feed_max); 
+
+                        // Build an array of all the items, starting with element 0 (first element).
+                        $items = $feed->get_items( 0, $maxitems );   
+                    }
+                }
+
+                if ( ! empty( $items ) ) {
+                    // Gather the permalinks of existing feed item's related to this feed source
+                    global $wpdb;
+                   
+
+                    foreach ( $items as $item ) {
+	
+
+
+                        // Check if newly fetched item already present in existing feed item item, 
+                        // if not insert it into wp_posts and insert post meta.
+
+					$mypostids = $wpdb->get_results("select post_id from $wpdb->postmeta where meta_key = 'rssmi_item_permalink' and meta_value like '%".$item->get_link()."%'");
+
+				//	$myposttitle=$wpdb->get_results("select post_title from $wpdb->posts where post_type='rssmi_feed_item' and post_title like '%".mysql_real_escape_string(trim($item->get_title()))."%'");
+							
+                       	if ((empty( $mypostids ) && $mypostids !== false)){ 
+	
+						$didUpdate=1;  //indicates this feed has an update
+						
+							if (IS_NULL($item->get_date())){
+								$post_date = $rightNow;  
+								$unix_date=strtotime($rightNow);
+					
+							}else{
+						  		$post_date = get_date_from_gmt( $item->get_date( 'Y-m-d H:i:s' ) ) ; 
+								$unix_date=$item->get_date( 'U' );
+								
+							}
+							
+						
+						
+                            // Create post object
+                            $feed_item = array(
+                                'post_title' => html_entity_decode($item->get_title()),
+                                'post_content' => '',
+								'post_date' =>$post_date, 
+                                'post_status' => 'publish',
+                                'post_type' => 'rssmi_feed_item'
+                            );                
+                            $inserted_ID = wp_insert_post( $feed_item, $wp_error );
+
+							
+							
+							if ($feedAuthor = $item->get_author())
+							{
+								$feedAuthor=$item->get_author()->get_name();
+							}
+							
+					
+								
+									
+									if ($enclosure = $item->get_enclosure())
+									{
+
+										$FeedMediaID=get_post_meta(  $feed_ID ,'rssmi_mediaID', true );// GET THE CHOSEN MEDIA IMAGE FROM THE ENCLOSURE
+										$useMediaImage= ($FeedMediaID > 0 ? 1 : 0);
+										$inum = (isset($FeedMediaID) ? $FeedMediaID-1 : 0); 
+										if(!IS_NULL($item->get_enclosure()->get_thumbnails()))
+											{
+												$mediaImageArray=$item->get_enclosure()->get_thumbnails();
+												$mediaImage=$mediaImageArray[$inum];
+
+											}
+												else if (!IS_NULL($item->get_enclosure()->get_link()))
+											{
+												$mediaImage=$item->get_enclosure()->get_link();	
+											}	
+										}
+										
+										
+										if (!IS_Null($item->get_categories())){	
+														$categoryTerms="";
+														foreach ($item->get_categories() as $category)
+														    {
+														    	$categoryTerms .= $category->get_term().', ';
+														    }
+														$postCategories=rtrim($categoryTerms,', ');
+											}else{
+													$postCategories=Null;
+											}
+									
+						
+						
+									if ($itemAuthor = $item->get_author())
+									{
+										$itemAuthor=(!IS_NULL($item->get_author()->get_name()) ? $item->get_author()->get_name() : $item->get_author()->get_email());
+									}else if (!IS_NULL($feedAuthor)){
+										$itemAuthor=$feedAuthor;
+							
+									}
+						
+						$myarray[]=array(
+							"mystrdate"=>strtotime($post_date),
+							"mytitle"=>html_entity_decode($item->get_title()),
+							"mylink"=>$item->get_link(),
+							"mydesc"=>$item->get_content(),
+							"myimage"=>$mediaImage,
+							"myAuthor"=>$itemAuthor,
+							"itemcategory"=>$postCategories,
+							"mycatid"=>$feed_cat,
+							"myGroup"=>$rssmi_title,
+							"feedID"=>$feed_ID,
+							"useMediaImage"=>$useMediaImage
+							);
+				
+									unset($mediaImage);
+									unset($itemAuthor);
+									unset($useMediaImage);
+									unset($post_date);
+									unset($rssmi_title);
+									unset($feed_cat);
+									
+                           	update_post_meta( $inserted_ID, 'rssmi_item_permalink', $item->get_permalink() );
+                            update_post_meta( $inserted_ID, 'rssmi_item_description', $myarray );                        
+                            update_post_meta( $inserted_ID, 'rssmi_item_date', $unix_date ); // Save as Unix timestamp format
+                            update_post_meta( $inserted_ID, 'rssmi_item_feed_id', $feed_ID);
+							unset($myarray);
+							unset($unix_date);
+							
+                       } //end if
+                    } //end foreach
+                } // end if
+					if ($didUpdate==1){
+						update_post_meta($feed_ID, 'rssmi_last_update', time()) ;
+					}
+					unset($feed_ID);
+					wp_reset_postdata(); // Restore the $post global to the current post in the main query
+
+            } // end $feed_sources while loop
+
+    } // end if
+
+	rssmi_update_feed_time();
+
+} 
+
+
+add_action('wp_insert_post', 'rssmi_fetch_feed_items'); 
+/**
+ * Fetches feed items for a specific feed
+ */
+function rssmi_fetch_feed_items( $post_id , $feed_total_fetch=10) { 
+	
+  	$directFetch=1; 
+	$timeout=20;
+	$forceFeed=true;
+	$showVideo=0;
+ 	global $wpdb;
+    $didUpdate=0;
+    $post = get_post( $post_id );
+     
+    if( ( $post->post_type == 'rssmi_feed' ) && ( $post->post_status == 'publish' ) ) { 
+        // Get the feed source
+
+		$query = "SELECT ID, post_date, post_title, guid FROM $wpdb->posts WHERE post_status = 'publish' AND post_type = 'rssmi_feed' AND ID = $post_id";
+		$feed_source =$wpdb->get_results($query);
+           
+        if( !empty($feed_source)) {
+                 
+                $feed_ID = $post_id;
+                $feed_url = get_post_meta( $post_id, 'rssmi_url', true );
+				$feed_cat = get_post_meta( $post_id, 'rssmi_cat', true );
+				$rssmi_user= get_post_meta($post_id, 'rssmi_user', true );
+				$rssmi_title= get_the_title($post_id);
+ 
+                if( !empty( $feed_url ) ) {             
+                    
+                		if ($directFetch==1){
+							$feed = wp_rss_fetchFeed($feed_url,$timeout,$forceFeed,$showVideo);
+						}else{
+							$feed = fetch_feed($feed_url);
+						}
+                    
+                    if ( !is_wp_error( $feed ) ) {
+                        // Limit  to 10 unless fetched from feed page. 
+                        $maxitems = $feed->get_item_quantity($feed_total_fetch); 
+
+                        // Build an array of all the items, starting with element 0 (first element).
+                        $items = $feed->get_items( 0, $maxitems );   
+                    }
+                    else { return; }
+                }       
+
+                if ( ! empty( $items ) ) {
+                    
+
+                    foreach ( $items as $item ) {
+
+                     	$mypostids = $wpdb->get_results("select post_id from $wpdb->postmeta where meta_key = 'rssmi_item_permalink' and meta_value like '%".$item->get_link()."%'");
+
+						//	$myposttitle=$wpdb->get_results("select post_title from $wpdb->posts where post_type='rssmi_feed_item' and post_title like '%".mysql_real_escape_string(trim($item->get_title()))."%'");
+							
+                       	if ((empty( $mypostids ) && $mypostids !== false)){
+	
+	   						$didUpdate=1;
+	
+							if (IS_NULL($item->get_date())){
+								$post_date = $rightNow;  
+								$unix_date=strtotime($rightNow);
+								
+							}else{
+						  		$post_date = get_date_from_gmt( $item->get_date( 'Y-m-d H:i:s' ) ) ;  
+								$unix_date=$item->get_date( 'U' );
+								
+							}
+	
+
+	
+                            // Create post object
+                            $feed_item = array(
+                                'post_title' => html_entity_decode($item->get_title()),
+                                'post_content' => '',
+								'post_date' =>$post_date, 
+                                'post_status' => 'publish',
+                                'post_type' => 'rssmi_feed_item'
+                            ); 
+               				remove_action('save_post', 'rssmi_save_custom_fields');
+                            $inserted_ID = wp_insert_post( $feed_item );
+							add_action( 'save_post', 'rssmi_save_custom_fields' ); 
+  							if ($feedAuthor = $item->get_author())
+								{
+									$feedAuthor=$item->get_author()->get_name();
+								}
+									
+										
+										if ($enclosure = $item->get_enclosure())
+										{
+
+											$FeedMediaID=get_post_meta(  $feed_ID ,'rssmi_mediaID', true );// GET THE CHOSEN MEDIA IMAGE FROM THE ENCLOSURE
+											$useMediaImage= ($FeedMediaID > 0 ? 1 : 0);
+											$inum = (isset($FeedMediaID) ? $FeedMediaID-1 : 0); 
+											if(!IS_NULL($item->get_enclosure()->get_thumbnails()))
+												{
+													$mediaImageArray=$item->get_enclosure()->get_thumbnails();
+													$mediaImage=$mediaImageArray[$inum];
+
+												}
+													else if (!IS_NULL($item->get_enclosure()->get_link()))
+												{
+													$mediaImage=$item->get_enclosure()->get_link();	
+												}	
+											}
+										
+								
+											if (!IS_Null($item->get_categories())){	
+															$categoryTerms="";
+															foreach ($item->get_categories() as $category)
+															    {
+															    	$categoryTerms .= $category->get_term().', ';
+															    }
+															$postCategories=rtrim($categoryTerms,', ');
+												}else{
+														$postCategories=Null;
+												}	
+										
+
+										if ($itemAuthor = $item->get_author())
+										{
+			$itemAuthor=(!IS_NULL($item->get_author()->get_name()) ? $item->get_author()->get_name() :$item->get_author()->get_email());
+										}else if (!IS_NULL($feedAuthor)){
+											$itemAuthor=$feedAuthor;
+										}
+
+										
+										$myarray[]=array(
+											"mystrdate"=>strtotime($post_date),
+											"mytitle"=>html_entity_decode($item->get_title()),
+											"mylink"=>$item->get_link(),
+											"mydesc"=>$item->get_content(),
+											"myimage"=>$mediaImage,
+											"myAuthor"=>$itemAuthor,
+											"itemcategory"=>$postCategories,
+											"mycatid"=>$feed_cat,
+											"myGroup"=>$rssmi_title,
+											"feedID"=>$feed_ID,
+											"useMediaImage"=>$useMediaImage
+											);
+
+													unset($mediaImage);
+													unset($itemAuthor);
+													unset($useMediaImage);
+													unset($post_date);
+													unset($rssmi_title);
+													unset($feed_cat);			
+
+	                            update_post_meta( $inserted_ID, 'rssmi_item_permalink', $item->get_permalink() );
+	                            update_post_meta( $inserted_ID, 'rssmi_item_description', $myarray );                        
+	                            update_post_meta( $inserted_ID, 'rssmi_item_date', $unix_date ); // Save as Unix timestamp format
+	                            update_post_meta( $inserted_ID, 'rssmi_item_feed_id', $post_id);
+								unset($myarray);
+								unset($unix_date);
+								
+           
+                       } //end if
+                    } //end foreach
+										
+                } // end if
+            } // end if not empty
+
+			if ($didUpdate==1){
+			update_post_meta($feed_ID, 'rssmi_last_update', time()) ;
+			}
+    } // end if
+} // end 
+
+
+
+
+
+?>
